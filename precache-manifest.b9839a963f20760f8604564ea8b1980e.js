112cfb25e73aed9c8c5e210c24c291710cb27ebe
sarahanamaria <110899240+sarahanamaria@users.noreply.github.com>
2022-11-18 17:20:59 +0200

    Updates

---
+++ precache-manifest.b9839a963f20760f8604564ea8b1980e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "546dcd0295f0c8251f96d4bdd256eaee",
+    "url": "/tips-app-react/index.html"
+  },
+  {
+    "revision": "670ade69126a1856e71f",
+    "url": "/tips-app-react/static/css/main.9c476414.chunk.css"
+  },
+  {
+    "revision": "6c5e6a74f698af3d0d7a",
+    "url": "/tips-app-react/static/js/2.1cfa60f4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tips-app-react/static/js/2.1cfa60f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "670ade69126a1856e71f",
+    "url": "/tips-app-react/static/js/main.57a655d0.chunk.js"
+  },
+  {
+    "revision": "5d558035520079d0581f",
+    "url": "/tips-app-react/static/js/runtime-main.0036498b.js"
+  },
+  {
+    "revision": "1c12a53ffc6cba4e148bd278e83b9b2e",
+    "url": "/tips-app-react/static/media/Medgidia.1c12a53f.jpg"
+  }
+]);
